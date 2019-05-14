755bfcd8e1035c64b3abebb148d0f89038bc50cb
andytan2624 <andy.tan2624@gmail.com>
2019-05-15 08:26:40 +1000

    modified code

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="Apptitle">Fib Calculator: Tang and Lime Soda</h1>
+            <h1 className="Apptitle">Fib Calculator: Tangenstein</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other page</Link>
           </header>
